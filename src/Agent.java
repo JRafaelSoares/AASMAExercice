4174c85554d851a74d0638b19254e926263c33cd
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-04 20:09:26 +0000

    Completed base Rational Agent
    Closes #1

--- src/Agent.java
+++ src/Agent.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Agent {
 
@@ -17,19 +15,26 @@ public class Agent {
 
 	public int restart = 0;
 
-	public double memoryFactor = 0.0;
+	public float memoryFactor = 0;
 
 	public String decision;
 
-	//Task implementation
-	public HashMap<String, Double> expectedValue = new HashMap<String, Double>();
+	/**********************************/
+	/*** A.1: Task Implementation *****/
+	/**********************************/
+	public HashMap<String, Float> expectedValue = new HashMap<>();
 
-	public HashMap<String, Double> realValue = new HashMap<String, Double>();
+	public HashMap<String, Float> realValue = new HashMap<>();
 
 	public String taskChosen;
 
-	public double total = 0;
+	public float total = 0;
 
+	/**********************************/
+	/******** A.2: Debugging  *********/
+	/**********************************/
+
+	public boolean debugging = false;
 
 	public Agent(String[] options) {
 		for (String option: options) {
@@ -42,8 +47,9 @@ public class Agent {
 
 		if(values[0].equals("A")){
 			//TODO - Case with multiple utilities for multiple tasks?
-			double utilityValue = Double.parseDouble(values[1].split("=")[1]);
+			float utilityValue = Float.parseFloat(values[1].split("=")[1]);
 
+			if(debugging) System.out.println(String.format(Locale.US,"Old value: %.1f New Value: %.1f", expectedValue.get(taskChosen), utilityValue));
 			//Updates utility value of task
 			this.expectedValue.replace(taskChosen, utilityValue);
 			this.realValue.replace(taskChosen, utilityValue);
@@ -54,8 +60,9 @@ public class Agent {
 
 		else{
 			//Gets value from tasks of type TX u=Y
-			this.expectedValue.put(values[0], Double.parseDouble(values[1].split("=")[1]));
-			this.realValue.put(values[0], Double.NEGATIVE_INFINITY);
+			this.expectedValue.put(values[0], Float.parseFloat(values[1].split("=")[1]));
+			if(debugging) System.out.println(String.format(Locale.US,"Task: %s Value: %.1f",values[0], Float.parseFloat(values[1].split("=")[1])));
+			this.realValue.put(values[0], Float.NEGATIVE_INFINITY);
 
 		}
 	}
@@ -74,6 +81,8 @@ public class Agent {
 		String line = br.readLine();
 		Agent agent = new Agent(line.split(" "));
 		while(!(line=br.readLine()).startsWith("end")) {
+			//DEBUGGING
+			if(agent.debugging) System.out.println(line);
 			if(line.startsWith("TIK")) agent.decideAndAct();
 			else agent.perceive(line);
 		}
@@ -86,8 +95,8 @@ public class Agent {
 	/******************************/
 	
 	public void maxUtil(){
-		double maxValue = Double.NEGATIVE_INFINITY;
-		double currentvalue;
+		float maxValue = Float.NEGATIVE_INFINITY;
+		float currentvalue;
 
 		for (String key: this.expectedValue.keySet()) {
 			currentvalue = this.expectedValue.get(key);
@@ -97,10 +106,12 @@ public class Agent {
 			}
 
 			//TODO - Check if compareTo is correct
-			else if (currentvalue == maxValue && this.taskChosen.compareTo(key) < 0){
+			else if (currentvalue == maxValue && this.taskChosen.compareTo(key) > 0){
 				this.taskChosen = key;
 			}
 		}
+
+		if(debugging) System.out.println(String.format(Locale.US,"Chosen task: %s Expected value: %.1f", this.taskChosen, maxValue));
 	}
 	
 	/******************************/
@@ -119,7 +130,7 @@ public class Agent {
 				this.restart = Integer.parseInt(initialization[1]);
 				break;
 			case "memory-factor":
-				this.memoryFactor = Double.parseDouble(initialization[1]);
+				this.memoryFactor = Float.parseFloat(initialization[1]);
 				break;
 		}
 	}
@@ -127,11 +138,13 @@ public class Agent {
 	public String getOutput(){
 		String output = "state={";
 
-		//TODO - Does a Set always keeps the same order?
-		for (String key: this.realValue.keySet()) {
-			double currentValue = this.realValue.get(key);
-			if(currentValue != Double.NEGATIVE_INFINITY){
-				output = output.concat(String.format("%s=%.2f,",key, currentValue));
+		List<String> list = new ArrayList<>(this.realValue.keySet());
+		//sort list of Tasks
+		java.util.Collections.sort(list);
+		for (String key: list) {
+			float currentValue = this.realValue.get(key);
+			if(currentValue != Float.NEGATIVE_INFINITY){
+				output = output.concat(String.format(Locale.US, "%s=%.1f,",key, currentValue));
 			}
 
 			else {
@@ -141,7 +154,7 @@ public class Agent {
 		//Removes last ","
 		output = output.substring(0, output.length()-1);
 
-		return output.concat(String.format("} gain=%.2f", this.total));
+		return output.concat(String.format(Locale.US,"} gain=%.1f", this.total));
 
 	}
 }

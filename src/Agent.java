e865d9ec064470636f563e1f8852796997057399
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-04 14:59:13 +0000

    First iteration of rationale agent added

--- src/Agent.java
+++ src/Agent.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Agent {
 
@@ -9,6 +11,8 @@ public class Agent {
 	/**********************************/
 	/******* A: AGENT BEHAVIOR ********/
 	/**********************************/
+
+	//Agent caractheristics
 	public int cycle;
 
 	public int restart = 0;
@@ -17,7 +21,15 @@ public class Agent {
 
 	public String decision;
 
-	public Map<String, Double> tasks;
+	//Task implementation
+	public HashMap<String, Double> expectedValue = new HashMap<String, Double>();
+
+	public HashMap<String, Double> realValue = new HashMap<String, Double>();
+
+	public String taskChosen;
+
+	public double total = 0;
+
 
 	public Agent(String[] options) {
 		for (String option: options) {
@@ -28,16 +40,29 @@ public class Agent {
 	public void perceive(String input) {
 		String[] values = input.split(" ");
 
-		//if(values[0].compareTo("A")){
+		if(values[0].equals("A")){
+			//TODO - Case with multiple utilities for multiple tasks?
+			double utilityValue = Double.parseDouble(values[1].split("=")[1]);
 
-		//}
+			//Updates utility value of task
+			this.expectedValue.replace(taskChosen, utilityValue);
+			this.realValue.replace(taskChosen, utilityValue);
+
+
+			total+= utilityValue;
+		}
+
+		else{
+			//Gets value from tasks of type TX u=Y
+			this.expectedValue.put(values[0], Double.parseDouble(values[1].split("=")[1]));
+			this.realValue.put(values[0], Double.NEGATIVE_INFINITY);
+
+		}
 	}
 	
-	public void decideAndAct() {}
+	public void decideAndAct() { maxUtil(); }
 	
-	public String recharge() {
-		return "output";
-	}
+	public String recharge() { return getOutput(); }
 
 	
 	/******************************/
@@ -57,7 +82,29 @@ public class Agent {
 	}
 
 	/******************************/
-	/******* C: AUX FUNCTIONS******/
+	/**** C: UTILITY FUNCTIONS*****/
+	/******************************/
+	
+	public void maxUtil(){
+		double maxValue = Double.NEGATIVE_INFINITY;
+		double currentvalue;
+
+		for (String key: this.expectedValue.keySet()) {
+			currentvalue = this.expectedValue.get(key);
+			if (currentvalue > maxValue){
+				maxValue = currentvalue;
+				this.taskChosen = key;
+			}
+
+			//TODO - Check if compareTo is correct
+			else if (currentvalue == maxValue && this.taskChosen.compareTo(key) < 0){
+				this.taskChosen = key;
+			}
+		}
+	}
+	
+	/******************************/
+	/******* D: AUX FUNCTIONS******/
 	/******************************/
 
 	public void initializationParse(String[] initialization){
@@ -75,5 +122,26 @@ public class Agent {
 				this.memoryFactor = Double.parseDouble(initialization[1]);
 				break;
 		}
+	}
+
+	public String getOutput(){
+		String output = "state={";
+
+		//TODO - Does a Set always keeps the same order?
+		for (String key: this.realValue.keySet()) {
+			double currentValue = this.realValue.get(key);
+			if(currentValue != Double.NEGATIVE_INFINITY){
+				output = output.concat(String.format("%s=%.2f,",key, currentValue));
+			}
+
+			else {
+				output = output.concat(String.format("%s=NA,", key));
+			}
+		}
+		//Removes last ","
+		output = output.substring(0, output.length()-1);
+
+		return output.concat(String.format("} gain=%.2f", this.total));
+
 	}
 }
